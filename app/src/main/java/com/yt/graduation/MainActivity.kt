d58e228c536984a70c07e5efe85a97e9b25b11b9
yusuf-teker <79746018+yusuf-teker@users.noreply.github.com>
2022-02-06 21:51:46 +0300

    Login and Register pages added

--- app/src/main/java/com/yt/graduation/MainActivity.kt
+++ app/src/main/java/com/yt/graduation/MainActivity.kt
@@ -2,18 +2,30 @@ package com.yt.graduation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        auth = FirebaseAuth.getInstance()
+        if ( auth.currentUser != null ){
+            val database = Firebase.database
+            val myRef = database.getReference("message")
 
-        val database = Firebase.database
-        val myRef = database.getReference("message")
+            myRef.setValue("Hello, Worl2gd!")
 
-        myRef.setValue("Hello, Worl2d!")
+            myRef.setValue("Hello, Worl2d3!")
+        }
+
+
+
+
     }
 }
