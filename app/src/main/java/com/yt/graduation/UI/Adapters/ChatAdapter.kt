84e2b112da922a23474ae3519f1e7a616771ada3
yusuf-teker <79746018+yusuf-teker@users.noreply.github.com>
2022-04-06 17:01:17 +0300

    message sent by the user align to right and message sent by receiver align to left

--- app/src/main/java/com/yt/graduation/UI/Adapters/ChatAdapter.kt
+++ app/src/main/java/com/yt/graduation/UI/Adapters/ChatAdapter.kt
@@ -6,7 +6,6 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -21,13 +20,13 @@ class ChatAdapter(private var messages: ArrayList<Message>,private val receiverI
         val chatMessage: TextView = itemView.findViewById(R.id.message)
         val timeToSend: TextView = itemView.findViewById(R.id.timeToSend)
         val messageRootLL: LinearLayout = itemView.findViewById(R.id.messageRootLL)
+        val justifyRight: View = itemView.findViewById(R.id.justifyRight)
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.list_item_chat_message, parent, false)
-
         return ViewHolder(itemView)
     }
 
@@ -38,8 +37,11 @@ class ChatAdapter(private var messages: ArrayList<Message>,private val receiverI
         holder.timeToSend.text = context.resources.getString(R.string.messageSendTime,message.timeToSend.subSequence(11,16),message.timeToSend.subSequence(5,7),message.timeToSend.subSequence(8,10))//LocalTime olarak geliyor
         if (message.senderId == receiverID){
             holder.messageRootLL.setBackgroundResource(R.drawable.message_background_receiver)
-
-        }else holder.messageRootLL.setBackgroundResource(R.drawable.message_background_sender)
+            holder.justifyRight.visibility = View.GONE
+        }else{
+            holder.messageRootLL.setBackgroundResource(R.drawable.message_background_sender)
+            holder.justifyRight.visibility = View.VISIBLE
+        }
     }
 
     override fun getItemCount(): Int {
